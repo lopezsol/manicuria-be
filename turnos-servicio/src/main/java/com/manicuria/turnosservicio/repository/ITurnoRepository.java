103b975398c6df1f27b49377e5ff69d7084a5113
lopezsol <mariasollopez23@gmail.com>
2024-07-19 05:43:30 -0300

    se modifican los endpoins traer y traer/dni para que esten ordenados en base a la fecha y hora

--- turnos-servicio/src/main/java/com/manicuria/turnosservicio/repository/ITurnoRepository.java
+++ turnos-servicio/src/main/java/com/manicuria/turnosservicio/repository/ITurnoRepository.java
@@ -4,6 +4,8 @@ import com.manicuria.turnosservicio.model.Turno;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 @Repository
